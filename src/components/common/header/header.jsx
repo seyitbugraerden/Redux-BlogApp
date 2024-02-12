df72d10967b8a31553de12b231e56bb4f5eaa849
Bugra <seyitbugraerden@gmail.com>
2024-02-12 14:11:52 +0300

    header Update

--- src/components/common/header/header.jsx
+++ src/components/common/header/header.jsx
@@ -3,12 +3,15 @@ import "./header.css";
 
 function Header() {
   return (
-    <>
+    <header>
       <img
         src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/5bfa2f97460951.5ec57c2734a00.jpg"
         alt=""
       />
-    </>
+      <div className="text-area">
+        <h1>Cornflakes and milk</h1>
+      </div>
+    </header>
   );
 }
 
